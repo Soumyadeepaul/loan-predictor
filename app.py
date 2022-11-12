da5d377735a805bc5212b6142e5f4ed26e389210
Soumyadeepaul <92711266+Soumyadeepaul@users.noreply.github.com>
2022-11-12 20:12:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ from flask import Flask,render_template,request
 import pandas as pd
 import pickle
 import gspread
+import sys
+import logging
 app=Flask(__name__)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 model=pickle.load(open('model.pkl','rb'))
 @app.route('/', methods=['GET','POST'])
 def form_detail():
